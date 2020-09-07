97730717fa421e19b9ff45f880c486099f3be2dd
ridha.zorgui <ridha.zorgui@infor.com>
2020-09-07 16:36:57 +0100

    fazet

--- docker-server/config.nix
+++ docker-server/config.nix
@@ -1,5 +1,5 @@
 { config, lib,  pkgs, resources, name, ... }:
 {
- environment.systemPackages = with pkgs; [ vim tree docker-compose ];
+ environment.systemPackages = with pkgs; [ vim tree docker-compose git awscli terraform nodejs ];
  virtualisation.docker.enable = true;
 }
